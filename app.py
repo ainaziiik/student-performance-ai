15feea9902e26d59c240d866ea16c53788824ca9
ainaziiik <166890029+ainaziiik@users.noreply.github.com>
2026-06-11 03:18:17 +0600

    Initialize Streamlit app with basic configuration

--- app.py
+++ app.py
@@ -1,1 +1,4 @@
-
+import streamlit as st
+st.set_page_config(page_title="Student AI System", layout="wide")
+st.title("📊 Student Performance AI System")
+st.write("Система успешно запущена. Если ты это видишь — Streamlit работает 😌")
