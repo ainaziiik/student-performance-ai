5f042e0bc573c54ddd508747ed6ba5e14ee84c65
ainaziiik <166890029+ainaziiik@users.noreply.github.com>
2026-06-11 04:00:08 +0600

    Implement correlation heatmap for UCI dataset
    Added correlation heatmap visualization for numeric data.

--- pages/2_UCI.py
+++ pages/2_UCI.py
@@ -1,5 +1,8 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
 st.title("🏫 UCI Student Performance Dataset")
 uploaded_file = st.file_uploader(
     "Загрузите UCI CSV файл",
@@ -26,3 +29,17 @@ if uploaded_file is not None:
     )
     st.subheader("Предпросмотр данных")
     st.dataframe(df.head(10))
+
+st.subheader("📊 Корреляционная тепловая карта")
+
+numeric_df = df.select_dtypes(include=[np.number])
+
+fig, ax = plt.subplots(figsize=(12, 6))
+
+sns.heatmap(
+    numeric_df.corr(),
+    cmap="magma",
+    ax=ax
+)
+
+st.pyplot(fig)
