b68832801e1d104fcd988630af90b0c605ae7cc7
ainaziiik <166890029+ainaziiik@users.noreply.github.com>
2026-06-11 03:41:02 +0600

    Change page title and enhance project introduction
    Updated page title and added project description.

--- app.py
+++ app.py
@@ -1,4 +1,24 @@
 import streamlit as st
-st.set_page_config(page_title="Student AI System", layout="wide")
+st.set_page_config(
+    page_title="Student Performance AI",
+    layout="wide"
+)
+def load_css():
+    with open("assets/style.css") as f:
+        st.markdown(
+            f"<style>{f.read()}</style>",
+            unsafe_allow_html=True
+        )
+
+load_css()
 st.title("📊 Student Performance AI System")
-st.write("Система успешно запущена. Если ты это видишь — Streamlit работает 😌")
+st.markdown("""
+### Искусственный интеллект для анализа успеваемости студентов
+
+Проект содержит:
+
+- 🏫 Анализ школьного датасета UCI
+- 🎓 Анализ университетского датасета PIP
+- ⚖️ Сравнение моделей
+- 🔮 Прогнозирование успеваемости
+""")
