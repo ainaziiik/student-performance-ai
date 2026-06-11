d33abdc26120a7b02695f1ec2f00111d48c51ebe
ainaziiik <166890029+ainaziiik@users.noreply.github.com>
2026-06-11 14:25:22 +0600

    Update 4_Comparison.py

--- pages/4_Comparison.py
+++ pages/4_Comparison.py
@@ -2,6 +2,15 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
+def load_css():
+    with open("assets/style.css") as f:
+        st.markdown(
+            f"<style>{f.read()}</style>",
+            unsafe_allow_html=True
+        )
+
+load_css()
+
 st.title("⚖️ Сравнение UCI и PIP")
 
 st.markdown("""
