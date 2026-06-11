5b535afb36384e867cae5b4fd23f2564e3bda50e
ainaziiik <166890029+ainaziiik@users.noreply.github.com>
2026-06-11 14:20:45 +0600

    Update 5_Predictor.py

--- pages/5_Predictor.py
+++ pages/5_Predictor.py
@@ -1,5 +1,13 @@
 import streamlit as st
 
+def load_css():
+    with open("assets/style.css") as f:
+        st.markdown(
+            f"<style>{f.read()}</style>",
+            unsafe_allow_html=True
+        )
+
+load_css()
 st.title("🔮 Student Performance Predictor")
 
 st.markdown("""
